ed488eda0e519f8b9a369d11d6c71562afc95cde
aamnawaheed <aamnawaheed88@gmail.com>
2025-11-28 03:38:52 +0500

    added configurations

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TMS; 
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to DI container
+builder.Services.AddScoped<UserService>();
+
+// Add controllers in a whole project e.g: userController
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Map controllers: to route HTTP requests to controller actions
+app.MapControllers();
+
+app.Run();
